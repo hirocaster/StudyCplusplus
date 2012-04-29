84e1bb384e87b299c51f624b8747cb5135cbace3
hirocaster <hirocast@gmail.com>
2012-04-30 03:21:05 +0900

    Add BikanBox

---
+++ mikanbox/mikanbox.cc
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+class MikanBox {
+public:
+  void Add(int addmikan);
+  void Del(int delmikan);
+  void Empty();
+  int GetTotal(){
+    return total;
+  }
+private:
+  int total;
+};
+
+void MikanBox::Add(int addmikan)
+{
+  total += addmikan;
+}
+
+void MikanBox::Del(int delmikan)
+{
+  total -= delmikan;
+  if(total < 0) Empty();
+}
+
+void MikanBox::Empty()
+{
+  total = 0;
+}
+
+int main()
+{
+  MikanBox myMikanBox;
+
+  myMikanBox.Empty();
+  myMikanBox.Add(5);
+  myMikanBox.Del(3);
+
+  printf("箱の中のみかん：%d個", myMikanBox.GetTotal());
+  return 0;
+}
